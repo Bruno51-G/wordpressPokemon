b88ddbe62da660857bef8c803b8a031c2451069f
Bruno51-G <185206289+Bruno51-G@users.noreply.github.com>
2026-03-23 03:45:03 +0100

    header, footer et function.php

---
+++ wp_app/wp-content/themes/monTheme/footer.php
@@ -0,0 +1,13 @@
+<!-- DEBUT DU FOOTER -->
+</main>
+
+<footer>
+    <p class="texteFooter">Bruno GODBILLOT Copyright CRM 2026</p>
+</footer>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
+<!-- FIN DU FOOTER -->
+
---
+++ wp_app/wp-content/themes/monTheme/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="favicon.png" />
+    <link rel="stylesheet" href="<?= get_stylesheet_uri() ?>">
+    <title>Document</title>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+<?php wp_body_open(); ?>
+
+<header class="site-header">
+    <p class="logoTitre"><?php bloginfo('name'); ?></p>
+</header>
+
+
+<main>
+
+<!-- FIN HEADER -->
